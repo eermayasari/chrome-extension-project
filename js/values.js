9df1e5b4e7f3083c4ac6bece6c7b60ab94c17756
Ella Ermayasari <e.ermayasari@gmail.com>
2018-12-29 11:44:21 +0300

    second chapter/commit

---
+++ js/values.js
@@ -0,0 +1,4 @@
+var projectName = 'kittenbook';
+var versionNumber = '0.0.1';
+var currentDate = new Date(); //Create Date object. More about objects and Date objects in Chapter 5. This object will be used to build our date.
+var currentTime = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate() + ' at ' + currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
